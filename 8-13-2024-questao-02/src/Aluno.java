f6101f1e8ae745e031077c349930c222305dc18a
José Bulhões <josebgojunior@gmail.com>
2024-08-15 21:29:56 -0300

    issae

--- 8-13-2024-questao-02/src/Aluno.java
+++ 8-13-2024-questao-02/src/Aluno.java
@@ -1,19 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
     String nome;
-    int matricula = 0;
-    double notas;
+    Integer matricula;
+    List<Double> notas;
 
-    public Aluno(String nome, int matricula, double notas) {
+    public Aluno(String nome, Integer matricula) {
         this.nome = nome;
         this.matricula = matricula;
-        this.notas = notas;
+        this.notas = new ArrayList<>();
     }
 
-    public void calcularMedia(){
-
+    public void adicionarNota(Double nota){
+        if ((nota > 0.0) && (nota < 10.0)) {
+            this.notas.add(nota);
+        }
     }
-    
+
+    public Double calcularMedia(){
+        Double media = 0.0;
+        for (Double nota : notas) {
+            media += nota;
+        }
+        return media / 3;
+    }
+
     public void verificarAprovacao(){
-        
+        if (calcularMedia() < 6.0) {
+            System.out.println("Reprovado");
+        }else{
+            System.out.println("Aprovado");
+        }
     }
 }
--- 8-13-2024-questao-02/src/App.java
+++ 8-13-2024-questao-02/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Aluno aluno = new Aluno("junior", 1);
+        aluno.adicionarNota(5.0);
+        aluno.adicionarNota(9.0);
+        aluno.adicionarNota(3.0);
+        aluno.calcularMedia();
+        aluno.verificarAprovacao();
     }
 }

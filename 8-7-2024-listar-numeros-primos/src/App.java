a5af6ec77a59de2ec1ca20f0286533337fa82a45
José Bulhões <josebgojunior@gmail.com>
2024-08-17 07:42:10 -0300

    junhonhonhon

---
+++ 8-7-2024-listar-numeros-primos/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Fala um número: ");
+        int quantidade = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i <= quantidade; i++) {
+            checarPrimo(i);
+        }
+        
+        sc.close();
+    }
+    public static void checarPrimo(int numPrimo){
+        int cont = 0;
+        for (int i = 1; i <= numPrimo; i++) {
+            if (numPrimo % i == 0) {
+                cont++;
+            }
+        }
+
+        if (cont == 2) {
+            System.out.println(numPrimo);                   
+        }
+    }
+}

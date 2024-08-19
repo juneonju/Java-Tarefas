25f0a0ec7fca10d5d425a1c77f0ce7f1ce8f2a12
José Bulhões <josebgojunior@gmail.com>
2024-08-19 17:56:06 -0300

    aaa

--- 8-16-2024-questao-02/src/App.java
+++ 8-16-2024-questao-02/src/App.java
@@ -3,8 +3,34 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Que ano estamos?");
-        Integer ano = sc.nextInt();
+        System.out.print("Que ano estamos?");
+        String ano = sc.nextLine();
+        String anoInvertido = "";
+
+        for (int i = ano.length()-1; i >= 0; i--) {
+            anoInvertido += ano.charAt(i);
+        }
+
+        String capturarValor = anoInvertido.substring(0,2);
+        String inverterValor = "";
+
+        for (int i = capturarValor.length()-1; i >= 0; i--) {
+            inverterValor += capturarValor.charAt(i);
+        }
+
+        int anoAtual = Integer.parseInt(ano);
+        int valor = Integer.parseInt(inverterValor);
+
+        if (anoAtual % 400 == 0) {
+            System.out.println("Ano bissexto");
+        }else if(valor == 00) {
+            System.out.println("Ano não é bissexto");
+        }else if (valor % 4 == 0) {
+            System.out.println("Ano bissexto");
+        }else{
+            System.out.println("O mundo acabou");
+        }
+
         sc.close();
     }
 }

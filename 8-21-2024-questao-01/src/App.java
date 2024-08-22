207f81db4125dbd2b76e19ed282946daec1eaa21
José Bulhões <josebgojunior@gmail.com>
2024-08-21 21:40:20 -0300

    salvo

--- 8-21-2024-questao-01/src/App.java
+++ 8-21-2024-questao-01/src/App.java
@@ -4,12 +4,26 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite números: ");
-        String numeros = sc.nextLine();
-        String[] array = numeros.split(",");
+        System.out.println("Digite uma lista de números separados por virgula: ");
+        String lista = sc.nextLine();
+        String[] array = lista.split(",");
         Integer[] numsInt = transformarArray(array);
         System.out.println(Arrays.toString(numsInt));
-        Arrays.sort(numsInt);
+
+        Integer[] novaLista = new Integer[numsInt.length];
+
+        for (int i = 0; i < numsInt.length; i++) {
+            
+            if (numsInt[i] == numsInt[i + 1]) {
+                novaLista[i] = numsInt[i + 1];
+                i = i + 2;
+            }else{
+                novaLista[i] = numsInt[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(novaLista));
+
         sc.close();
     }
 

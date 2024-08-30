ab9804c7df77da422601be9ec9c48aaf5422fa15
José Bulhões <josebgojunior@gmail.com>
2024-08-30 19:57:05 -0300

    listaCompleta

--- 8-28-2024-questao-01/src/App.java
+++ 8-28-2024-questao-01/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Store loja = new Store();
+        Product produto1 = new Product("Camisa", 50.0);
+        Product produto2 = new Product("Calça", 100.0);
+        Request pedido1 = new Request("01");
+        loja.addProduct(produto1);
+        System.out.println(produto1.name);
+        System.out.println(produto1.price);
+        loja.addProduct(produto2);
+        System.out.println(produto2.name);
+        System.out.println(produto2.price);
+        loja.createRequest(pedido1);
+        System.out.println(pedido1.id);
+        loja.addProductToRequest(produto1, "01");
+        System.out.println(loja.listRequests());
+        System.out.println(loja.calculateTotalRequest("01"));
     }
 }
--- 8-28-2024-questao-01/src/Store.java
+++ 8-28-2024-questao-01/src/Store.java
@@ -1,26 +1,49 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Store {
     List<Product> products;
     List<Request> requests;
 
+    public Store() {
+        products = new ArrayList<>();
+        requests = new ArrayList<>();
+    }
+
     public void addProduct(Product product){
-        this.products.add(product);
+        products.add(product);
     }
 
-    public void createRequest(String id){
-        
+    public void createRequest(Request request){
+        requests.add(request);
     }
 
-    public void addProductToRequest(Strind idRequest, Product product){
-
+    public void addProductToRequest(Product product, String id){
+        if (findRequestById(id) != null) {
+            findRequestById(id).products.add(product);
+        }
     }
 
     public Double calculateTotalRequest(String idRequest){
-
+        Double priceRequest = 0.0;
+            if (findRequestById(idRequest) != null) {
+                for (int i = 0; i < products.size(); i++) {
+                    priceRequest += products.get(i).price;
+                }
+            }
+        return priceRequest;
     }
 
     public List<Request> listRequests(){
         return requests;
+    }
+
+    public Request findRequestById(String id){
+        for (Request request : requests) {
+            if (request.id.equals(id)) {
+                return request;
+            }
+        }
+        return null;
     }
 }
